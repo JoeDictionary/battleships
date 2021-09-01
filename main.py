db27f6ba2e3875a49bd7284eda20be1bdff28db4
FilipFit <filipfit6@gmail.com>
2021-09-01 17:01:35 +0200

    sync

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ UR_CORNER = '╗'
 DL_CORNER = '╚'
 DR_CORNER = '╝'
 SHOT = 'x'
+SHIP_BODY = '█'
+EMPTY = ' '
 
 
 class Battleship():
 
     # TODO Move to build method?
-    direction_modifiers = {'N': (0, 1), 'S': (
-        0, -1), 'E': (-1, 0), 'W': (1, 0)}
+    direction_modifiers = {'N': (0, -1), 'S': (
+        0, 1), 'E': (1, 0), 'W': (-1, 0)}
 
     @staticmethod
     def build(head, length, direction):
@@ -29,11 +31,11 @@ class Battleship():
         #         for i in range(length)]
         return body
 
-    def __init__(self, body):
-        self.body = body
+    def __init__(self, head, length, direction):
+        self.body = Battleship.build(head, length, direction)
 
 
-def render_box(width, height, shots):
+def render_shots(width, height, shots):
     shots = set(shots)
     print(' ' + UL_CORNER + HORIZONTAL * width + UR_CORNER)
 
@@ -46,11 +48,36 @@ def render_box(width, height, shots):
     print(' ' + DL_CORNER + HORIZONTAL * width + DR_CORNER)
 
 
+def render_battleships(board_width, board_height, battleships):
+    print("-------------")
+    # Init empty board
+    board = [[EMPTY for _ in range(board_width)] for _ in range(board_height)]
+
+    for ship in battleships:
+        for x, y in ship.body:
+            board[y][x] = SHIP_BODY
+
+    for row in board:
+        print(''.join(row))
+
+    print("-------------")
+
+    # return board
+
+
 if __name__ == '__main__':
+
+    battleships = [
+        Battleship((1, 1), 4, 'S')
+    ]
+
+    render_battleships(5, 10, battleships)
+
+    exit(0)
+
     shots = []
-    # while True:
-    #     xstr, ystr = input("Shot coordinates:\n").split(',')
-    #     x, y = int(xstr), int(ystr)
-    #     shots.append((x, y))
-    #     render_box(15, 10, shots)
-    print(Battleship.build((1, 1), 4, 'N'))
+    while True:
+        xstr, ystr = input("Shot coordinates:\n").split(',')
+        x, y = int(xstr), int(ystr)
+        shots.append((x, y))
+        render_shots(15, 10, shots)
