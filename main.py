37c8a8b3459ca596fa96df6634b201c4f684e9d9
FilipFit <filipfit6@gmail.com>
2021-09-03 00:38:00 +0200

    render_battleships function added

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-HORIZONTAL = '═'
+HORIZONTAL = '══'
 VERTICAL = '║'
-UL_CORNER = '╔'
-UR_CORNER = '╗'
-DL_CORNER = '╚'
-DR_CORNER = '╝'
-SHOT = 'x'
 EMPTY = '  '
 SHIP_BODY = '██'
+NW_CORNER = '╔'  # north-west corner
+NE_CORNER = '╗'  # north-east corner
+SW_CORNER = '╚'  # south-west corner
+SE_CORNER = '╝'  # south-east corner
+SHOT = 'x'
 
 
 class Battleship():
@@ -34,33 +34,35 @@ class Battleship():
 
 def render_shots(width, height, shots):
     shots = set(shots)
-    print(' ' + UL_CORNER + HORIZONTAL * width + UR_CORNER)
+    print(EMPTY + NW_CORNER + HORIZONTAL * width + NE_CORNER)
 
     for y in range(height):
         row = ""
         for x in range(width):
-            row += SHOT if (x, y) in shots else ' '
+            row += SHOT if (x, y) in shots else EMPTY
         print(str(y) + VERTICAL + row + VERTICAL)
 
-    print(' ' + DL_CORNER + HORIZONTAL * width + DR_CORNER)
+    print(EMPTY + SW_CORNER + HORIZONTAL * width + SE_CORNER)
 
 
-def render_battleship(board_width, board_height, ships):
+def render_battleships(board_width, board_height, ships):
     board = [[EMPTY for _ in range(board_width)]
              for _ in range(board_height)]
 
     for ship in ships:
-        for part in ship.body:
-            board[part[1]][part[0]] = SHIP_BODY
+        for x, y in ship.body:
+            board[y][x] = SHIP_BODY
 
+    print(NW_CORNER + HORIZONTAL * board_width + NE_CORNER)
     for row in board:
         print(VERTICAL + ''.join(row) + VERTICAL)
+    print(SW_CORNER + HORIZONTAL * board_width + SE_CORNER)
 
 
 if __name__ == '__main__':
     battleships = [Battleship((1, 1), 4, 's'), Battleship((3, 5), 2, 'w')]
 
-    render_battleship(15, 15, battleships)
+    render_battleships(15, 15, battleships)
 
     exit(0)
 
